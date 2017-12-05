4f4916f8d475b6d0c9ddf2e304075e15879389f0
benscott <ben@benscott.co.uk>
2017-12-05 12:40:58 +0000

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # data-portal-deployment
-Automated deployment tool for the Data Portal
+
+### Deployment tool for the Data Portal. 
+
+### Overview
+
+Script loops through all repositories defined in config - `default.cfg:main.directory`. 
+
+If the repository is a CKAN plugin owned by the Natural History Museum organisation, the script pulls either the latest master (for staging / development) or latest release (for production).
+
+### CLI
+
+To update all repositories to latest commit on master branch:
+
+```bash
+deploy master
+```
+
+To update all repositories to latest tagged release:
+
+```bash
+deploy release
+```
+
+Note: Repositories without a tagged release are not updated.
+
+To update a particular repository only:
+
+```bash
+deploy -n ckanext-nhm master
+```
+
+
+### Automation
+
+On staging `deploy master` is run on cron every 5 minutes. If source code changes are detected, Apache WSGI will be restarted so all updates merged into master are automatically deployed onto staging.
+
+On production `deploy release` and Apache WSGI will need to be manually run. 
--- deployment/cli.py
+++ deployment/cli.py
@@ -3,60 +3,10 @@
 """
 Created by Ben Scott on '16/11/2017'.
 """
-import os
+
 import click
-import ConfigParser
 
-from git.exc import InvalidGitRepositoryError
-
-from deployment.repository import Repository
-from deployment.exceptions import DeploymentException
 from deployment.deploy import Deploy
-
-
-
-#
-#
-# def update_repo_to_latest_release(repo):
-#     '''
-#     Update a repository to the latest release
-#     @param repo:
-#     @type Repository:
-#     @return:
-#     @rtype:
-#     '''
-#
-#
-# def update_repo_to_master(repo):
-#     '''
-#     Update a repository from master branch
-#     @param repo:
-#     @type Repository:
-#     @return:
-#     @rtype:
-#     '''
-#
-
-#
-#
-# @click.command()
-# @click.option('--latest_release', '-r', is_flag=True)
-
-# def main(latest_release, repo_name):
-#     for repo in list_repositories():
-#
-#         # If we only want to update one repo, skip if it doesn't match repo_name
-#         if repo_name and repo.name != repo_name:
-#             continue
-#
-#         if latest_release:
-#             update_repo_to_latest_release(repo)
-#         else:
-#             # Need to identify if the repo's been changed
-#             # ANd only restart apache if it has been
-#             update_repo_to_master(repo)
-#
-#             click.secho('Repositories updated - please restart WSGI to release changes:\n\n\ttouch /etc/ckan/default/apache.wsgi\n', fg='green')
 
 
 @click.group()
